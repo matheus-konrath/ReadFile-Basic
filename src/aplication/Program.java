a7a79237d19ae50e8b2180cd69772f93fede4918
Matheus Konrath <matheus.konrath@compasso.com.br>
2022-01-07 11:14:01 -0300

    FileReader and BufferedReader

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,31 +1,43 @@
 package aplication;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		File file = new File("C:\\Users\\matheus.konrath\\eclipse-workspace\\in.txt");
-		Scanner sc = null;
+		String path = ("C:\\Users\\matheus.konrath\\eclipse-workspace\\aulao002\\in.txt");
+		
+		FileReader fr = null;
+		BufferedReader br = null;
+		
 		try {
-			 sc = new Scanner(file);
-			 while(sc.hasNextLine()) {		//Traverse the Text
-				 String line = sc.nextLine(); // read line and go to the next
-				 System.out.println(line);
-			 }
-		}
-		catch(IOException e) {
-			System.out.println(e.getMessage());
-		}
-		finally {
-			if(sc != null) {
-			sc.close();
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine(); // read line
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 		}
+		catch(IOException e) {
+			System.out.println("Error" + e.getMessage());
+		}
 		
+		finally{
+			try {
+			br.close();
+			fr.close(); 
+			}
+		
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	}
 
