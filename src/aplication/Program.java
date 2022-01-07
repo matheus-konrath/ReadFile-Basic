56e3a9b8753043540d2be7032743f72097f4da95
Matheus Konrath <matheus.konrath@compasso.com.br>
2022-01-07 15:39:31 -0300

    lendo arquivo Csv e instanciando uma lista de objetos

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -3,6 +3,10 @@ package aplication;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
 
 
 public class Program {
@@ -11,20 +15,34 @@ public class Program {
 		
 		String path = ("C:\\Users\\matheus.konrath\\eclipse-workspace\\aulao002\\in.txt");
 		
-		
+		List<Product> list = new ArrayList<Product>(); 
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 				
 			String line = br.readLine(); // read line
+			line = br.readLine();
 			while (line != null) {
-				System.out.println(line);
+				
+				String[] vect = line.split(",");
+				String name = vect[0];
+				double price = Double.parseDouble (vect[1]);
+				Integer qte = Integer.parseInt(vect[2]);
+				
+				Product prod = new Product(name,price,qte);
+				list.add(prod);
+				
 				line = br.readLine();
+				}
+			System.out.println("Products: ");
+			for (Product p : list) {		// para cada produto p da minha lista
+				System.out.println(p);
 			}
+			
 		}
 		catch(IOException e) {
 			System.out.println("Error" + e.getMessage());
 		}
-		
+		 
 
 	}
 
---
+++ src/entities/Product.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private double price;
+	private Integer quantity;
+	
+	public Product() {
+		
+	}
+
+	public Product(String name, double price, Integer quantity) {
+		super();
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+	
+	
+	
+	
+}
